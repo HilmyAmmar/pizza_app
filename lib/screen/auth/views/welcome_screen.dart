f674ea1bf8e73fd9d1cf3d97cc6dce12a56c2114
HilmyAmmar <hilmyammardarmawan17@gmail.com>
2024-07-28 01:45:46 +0700

    feat: making UI progress for welcome page

--- lib/screen/auth/views/welcome_screen.dart
+++ lib/screen/auth/views/welcome_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -10,6 +13,70 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: const Color(0xfff2f0f3),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: height * 0.05),
+              height: MediaQuery.of(context).size.height * 0.4,
+              width: MediaQuery.of(context).size.width,
+              child: SvgPicture.asset(
+                "assets/ubisoft-seeklogo.svg",
+                fit: BoxFit.cover,
+              ),
+            ),
+            ClipPath(
+              clipper: WaveClipperTwo(reverse: true, flip: false),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.6,
+                width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.1),
+                decoration: const BoxDecoration(
+                  color: Color(0xff1B033D),
+                ),
+                child: Column(
+                  children: [
+                    Column(
+                      children: [
+                        const Text(
+                          "Hello!",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Expanded(
+                          child: Padding(
+                            padding:
+                                EdgeInsets.symmetric(horizontal: width * 0.1),
+                            child: const Text(
+                              "Welcome to the ultimate gaming experience! Play the games and connect with people around the world with Ubisoft Connect",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
